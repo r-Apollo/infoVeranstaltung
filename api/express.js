10e46deb0b43e877ec883d8b402264f8bfe81c6f
Apollo <rapollo@protonmail.com>
2022-01-05 10:29:22 +0100

    added 404

--- api/express.js
+++ api/express.js
@@ -4,8 +4,11 @@ import anmeldeRoutes from "./routes/anmeldung.routes.js"
 import cors from "cors";
 import dotenv from "dotenv";
 import "./server.js"
+import path from 'path';
 
 dotenv.config();
+const moduleURL = new URL(import.meta.url);
+const __dirname = path.dirname(moduleURL.pathname);
 
 const app = express();
 
@@ -15,6 +18,10 @@ app.use(cors())
 app.use("/api/events", eventRoutes)
 
 app.use("/api/anmeldung", anmeldeRoutes)
+
+app.get("*", (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '/index.html'))
+})
 
 app.listen(process.env.PORT | 8001,  () => {
     console.info(`Api running on http://localhost:${process.env.PORT | 8001}.`)
